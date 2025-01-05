71afab49f18b0e56752a2f3fbe83a43e780e0b91
NinjolasNJM <NinjolasNJM2003@gmail.com>
2025-01-04 20:26:41 -0600

    more work done

--- src/generators/minecraftModularBendable/minecraftModularBendableGenerator.ts
+++ src/generators/minecraftModularBendable/minecraftModularBendableGenerator.ts
@@ -29,8 +29,7 @@ const id = "minecraft-modular-bendable";
 
 const name = "Minecraft Modular Bendable Character";
 
-const history: HistoryDef = [
-];
+const history: HistoryDef = [];
 
 const thumbnail: ThumbnailDef = {
   url: thumbnailImage.src,
@@ -73,11 +72,14 @@ const script: ScriptDef = (generator: Generator) => {
 
   generator.defineSelectInput("Skin Model Type", ["Steve", "Alex"]);
   generator.defineBooleanInput("Show Folds", true);
+  generator.defineBooleanInput("Show Foreground", true);
+  generator.defineBooleanInput("Show Texture", true);
   /* generator.defineBooleanInput("Show Labels", true); */
 
   // Get user variable values
 
-  const isAlexModel = generator.getSelectInputValue("Skin Model Type") === "Alex";
+  const isAlexModel =
+    generator.getSelectInputValue("Skin Model Type") === "Alex";
   const showHeadOverlay = true;
   const showBodyOverlay = true;
   //const showLeftArmOverlay = true;
@@ -86,6 +88,8 @@ const script: ScriptDef = (generator: Generator) => {
   const showRightLegOverlay = true;
 
   const showFolds = generator.getBooleanInputValue("Show Folds");
+  const showForeground = generator.getBooleanInputValue("Show Foreground");
+  const showTexture = generator.getBooleanInputValue("Show Texture");
   /*const showColorCodes = generator.getBooleanInputValue("Show Color Codes");
   const showLabels = generator.getBooleanInputValue("Show Labels");*/
 
@@ -114,12 +118,20 @@ const script: ScriptDef = (generator: Generator) => {
 
   function drawHead([ox, oy]: Position) {
     const scale: Dimensions = [64, 64, 64];
-    minecraftGenerator.drawCuboid("Skin", char.base.head, [ox, oy], scale, { orientation: "South" });
+    minecraftGenerator.drawCuboid("Skin", char.base.head, [ox, oy], scale, {
+      orientation: "South",
+    });
     if (showHeadOverlay) {
-      minecraftGenerator.drawCuboid("Skin", char.overlay.head, [ox, oy], scale, {orientation: "South"});
+      minecraftGenerator.drawCuboid(
+        "Skin",
+        char.overlay.head,
+        [ox, oy],
+        scale,
+        { orientation: "South" }
+      );
     }
   }
-  
+
   function drawBody([ox, oy]: Position) {
     const scale: Dimensions = [64, 96, 32];
     minecraftGenerator.drawCuboid("Skin", char.base.body, [ox, oy], scale);
@@ -127,7 +139,7 @@ const script: ScriptDef = (generator: Generator) => {
       minecraftGenerator.drawCuboid("Skin", char.overlay.body, [ox, oy], scale);
     }
   }
-  
+
   /*function drawBody2([ox, oy]: Position) {
     const scale: Dimensions = [64, 96, 32];
     minecraftGenerator.drawCuboid("Skin", char.base.body, [ox, oy], scale, { center: "Bottom" });
@@ -135,15 +147,20 @@ const script: ScriptDef = (generator: Generator) => {
       minecraftGenerator.drawCuboid("Skin", char.overlay.body, [ox, oy], scale);
     }
   }*/
-  
+
   function drawRightArm([ox, oy]: Position) {
     const scale: Dimensions = char === alex ? [24, 96, 32] : [32, 96, 32];
     minecraftGenerator.drawCuboid("Skin", char.base.rightArm, [ox, oy], scale);
     if (showRightArmOverlay) {
-      minecraftGenerator.drawCuboid("Skin", char.overlay.rightArm, [ox, oy], scale);
+      minecraftGenerator.drawCuboid(
+        "Skin",
+        char.overlay.rightArm,
+        [ox, oy],
+        scale
+      );
     }
   }
-  
+
   /*function drawLeftArm([ox, oy]: Position) {
     const scale: Dimensions = char === alex ? [24, 96, 32] : [32, 96, 32];
     minecraftGenerator.drawCuboid("Skin", char.base.leftArm, [ox, oy], scale, { orientation: "East" });
@@ -151,71 +168,89 @@ const script: ScriptDef = (generator: Generator) => {
       minecraftGenerator.drawCuboid("Skin", char.overlay.leftArm, [ox, oy], scale, { orientation: "East" });
     }
   }*/
-  
+
   function drawRightLeg([ox, oy]: Position) {
     const scale: Dimensions = [32, 96, 32];
-    minecraftGenerator.drawCuboid(
-      "Skin",
-      char.base.rightLeg,
-      [ox, oy],
-      scale,
-      { center: "Left", orientation: "East" }
-    );
+    minecraftGenerator.drawCuboid("Skin", char.base.rightLeg, [ox, oy], scale, {
+      center: "Left",
+      orientation: "East",
+    });
     if (showRightLegOverlay) {
-      minecraftGenerator.drawCuboid("Skin", char.overlay.rightLeg, [ox, oy], scale);
+      minecraftGenerator.drawCuboid(
+        "Skin",
+        char.overlay.rightLeg,
+        [ox, oy],
+        scale
+      );
     }
   }
-  
+
   function drawLeftLeg([ox, oy]: Position) {
     const scale: Dimensions = [32, 96, 32];
-    minecraftGenerator.drawCuboid("Skin", char.base.leftLeg, [ox, oy], scale, { center: "Right" });
+    minecraftGenerator.drawCuboid("Skin", char.base.leftLeg, [ox, oy], scale, {
+      center: "Right",
+    });
     if (showLeftLegOverlay) {
-      minecraftGenerator.drawCuboid("Skin", char.overlay.leftLeg, [ox, oy], scale, { orientation: "East" });
+      minecraftGenerator.drawCuboid(
+        "Skin",
+        char.overlay.leftLeg,
+        [ox, oy],
+        scale,
+        { orientation: "East" }
+      );
     }
   }
-  
+
   function drawPage1() {
     generator.usePage("Page 1");
     generator.drawImage("Guide-1", [0, 0]);
-    drawHead([156, 28]); // Head
-    drawBody([188, 316]); // Body
-    generator.drawTexture("Skin", [16, 20, 4, 4], [116, 348, 32, 32]); // Right Square
-    generator.drawTexture("Skin", [28, 20, 4, 4], [404, 324, 32, 32]); // Left Square
-    generator.drawTexture("Skin", [53, 21, 2, 2], [26, 490, 18, 18]); // Right Back Circle
-    generator.drawTexture("Skin", [45, 53, 2, 2], [66, 490, 18, 18]); // Left Back Circle
-    generator.drawTexture("Skin", [44, 20, 4, 4], [424, 458, 32, 32]); // Right Front Circle
-    generator.drawTexture("Skin", [36, 52, 4, 4], [66, 490, 32, 32]); // Left Front Circle
+    if (showTexture) {
+      drawHead([156, 28]); // Head
+      drawBody([188, 316]); // Body
+      generator.drawTexture("Skin", [16, 20, 4, 4], [116, 348, 32, 32]); // Right Square
+      generator.drawTexture("Skin", [28, 20, 4, 4], [404, 324, 32, 32]); // Left Square
+      generator.drawTexture("Skin", [53, 21, 2, 2], [26, 490, 18, 18]); // Right Back Circle
+      generator.drawTexture("Skin", [45, 53, 2, 2], [66, 490, 18, 18]); // Left Back Circle
+      generator.drawTexture("Skin", [28, 24, 4, 4], [465, 457, 32, 32]); // Right Front Circle
+      generator.drawTexture("Skin", [16, 24, 4, 4], [433, 457, 32, 32]); // Left Front Circle
+    }
+    if (showForeground) {
+      generator.drawImage("Foreground-1", [0, 0]);
+    }
     if (showFolds) {
       generator.drawImage("Folds-1", [0, 0]);
     }
   }
-  
+
   function drawPage2() {
     generator.usePage("Page 2");
     generator.drawImage("Guide-2", [0, 0]);
     drawRightLeg([36, 484]);
     drawLeftLeg([172, 484]);
-    generator.drawImage("Foreground-2", [0, 0]);
+    if (showForeground) {
+      generator.drawImage("Foreground-2", [0, 0]);
+    }
     if (showFolds) {
       generator.drawImage("Folds-2", [0, 0]);
     }
   }
-  
+
   function drawPage3() {
     generator.usePage("Page 3");
     generator.drawImage("Guide-3", [0, 0]);
     drawRightArm([28, 148]);
+    if (showForeground) {
+      generator.drawImage("Foreground-3", [0, 0]);
+    }
     if (showFolds) {
       generator.drawImage("Folds-3", [0, 0]);
     }
   }
-  
+
   // Draw Pages
   drawPage1();
   drawPage2();
   drawPage3();
-
-
 
   // Labels
 
