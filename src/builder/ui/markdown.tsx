5a42a21083d1db4dcb5fa3c490d393ed331786cc
Pixel Papercraft <pixelpapercraft@gmail.com>
2026-03-01 10:59:26 +1100

    Update markdown renderer

--- src/builder/ui/markdown.tsx
+++ src/builder/ui/markdown.tsx
@@ -2,5 +2,7 @@ import { parse } from "marked";
 
 export function Markdown({ children }: { children: string }) {
   const html = parse(children);
+  // TODO: Handle async parsing from `marked.parse` and ensure this value is always a string.
+  // @ts-expect-error `marked.parse` can return Promise<string> in current typings.
   return <div className="prose" dangerouslySetInnerHTML={{ __html: html }} />;
 }
