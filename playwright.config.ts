ec262ef6d7099d31cf8e8fb2fbd0b0830cb4ca5d
Pixel Papercraft <pixelpapercraft@gmail.com>
2026-03-22 18:03:32 +1100

    Reuse existing Playwright web server

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   ],
   webServer: {
     command: 'npm run dev -- --hostname 127.0.0.1',
-    reuseExistingServer: false,
+    reuseExistingServer: true,
     timeout: 120000,
     url: 'http://127.0.0.1:3000',
   },
