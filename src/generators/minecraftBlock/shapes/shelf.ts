b91719ec8cbdbb760ab42a523ff9fbe80a9f7001
NinjolasNJM <NinjolasNJM2003@gmail.com>
2026-05-14 11:28:49 -0500

    all dests are in.

--- src/generators/minecraftBlock/shapes/shelf.ts
+++ src/generators/minecraftBlock/shapes/shelf.ts
@@ -28,24 +28,24 @@ const size = 128;
 
 function makeFaces(ox: number, oy: number): Faces {
   return {
-    top: [ox + size, oy + size * 11 / 16, size, size * 3 / 16],
-    bottom: [ox + size, oy + size * 38 / 16, size, size * 3 / 16],
-    right: [ox + size * 11 / 16, oy + size, size * 3 / 16, size],
-    front: [ox + size, oy + size * 22 / 16, size, size / 2],
-    left: [ox + size * 34 / 16, oy + size, size * 3 / 16, size],
-    back: [ox + size * 37 / 16, oy + size, size, size],
+    top: [ox + size, oy + (size * 11) / 16, size, (size * 3) / 16],
+    bottom: [ox + size, oy + (size * 38) / 16, size, (size * 3) / 16],
+    right: [ox + (size * 11) / 16, oy + size, (size * 3) / 16, size],
+    front: [ox + size, oy + (size * 22) / 16, size, size / 2],
+    left: [ox + (size * 34) / 16, oy + size, (size * 3) / 16, size],
+    back: [ox + (size * 37) / 16, oy + size, size, size],
 
-        top0: [ox + size, oy + size * 5 / 16, size, size * 3 / 16],
-    bottom0: [ox + size, oy + size * 2, size, size],
-    right0: [ox, oy + (size * 3) / 2, size, size / 2],
-    front0: [ox + size, oy + (size * 3) / 2, size, size / 2],
-    left0: [ox + size * 2, oy + (size * 3) / 2, size, size / 2],
+    top0: [ox + size, oy + (size * 30) / 16, size, (size * 2) / 16],
+    bottom0: [ox + size, oy + (size * 36) / 16, size, (size * 2) / 16],
+    right0: [ox + (size * 14) / 16, oy + size * 7 / 4, (size * 2) / 16, size / 4],
+    front0: [ox + size, oy + size * 2, size, size / 4],
+    left0: [ox + size * 2, oy + size * 7 / 4, (size * 2) / 16, size / 4],
 
-        top1: [ox + size, oy + size * 5 / 16, size, size * 3 / 16],
-    bottom1: [ox + size, oy + size * 2, size, size],
-    right1: [ox, oy + (size * 3) / 2, size, size / 2],
-    front1: [ox + size, oy + (size * 3) / 2, size, size / 2],
-    left1: [ox + size * 2, oy + (size * 3) / 2, size, size / 2],
+    top1: [ox + size, oy + (size * 14) / 16, size, (size * 2) / 16],
+    bottom1: [ox + size, oy + (size * 20) / 16, size, (size * 2) / 16],
+    right1: [ox + (size * 14) / 16, oy + size, (size * 2) / 16, size / 4],
+    front1: [ox + size, oy + size, size, size / 4],
+    left1: [ox + size * 2, oy + size, (size * 2) / 16, size / 4],
   };
 }
 
@@ -71,6 +71,30 @@ export function drawShelf(
   Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.front);
   Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.left);
   Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.back);
+
+  Face.defineInputRegion(generator, "ShelfFace" + blockId, regions.front);
+
+  Face.drawFace(generator, "ShelfFace" + blockId, [0, 0, 16, 16], regions.top0);
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [0, 0, 16, 16],
+    regions.bottom0
+  );
+  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.right0);
+  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.front0);
+  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.left0);
+
+    Face.drawFace(generator, "ShelfFace" + blockId, [0, 0, 16, 16], regions.top1);
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [0, 0, 16, 16],
+    regions.bottom1
+  );
+  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.right1);
+  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.front1);
+  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.left1);
 
   generator.drawImage("Tabs-Shelf", [ox - 32, oy - 1]);
 
