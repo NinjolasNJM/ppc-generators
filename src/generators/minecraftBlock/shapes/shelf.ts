4b37adb8fa8231c64365cdfe62cfcfbd1af091ec
NinjolasNJM <NinjolasNJM2003@gmail.com>
2026-05-14 11:54:16 -0500

    All sources mapped- but front is not yet changeable to all types

--- src/generators/minecraftBlock/shapes/shelf.ts
+++ src/generators/minecraftBlock/shapes/shelf.ts
@@ -37,9 +37,14 @@ function makeFaces(ox: number, oy: number): Faces {
 
     top0: [ox + size, oy + (size * 30) / 16, size, (size * 2) / 16],
     bottom0: [ox + size, oy + (size * 36) / 16, size, (size * 2) / 16],
-    right0: [ox + (size * 14) / 16, oy + size * 7 / 4, (size * 2) / 16, size / 4],
+    right0: [
+      ox + (size * 14) / 16,
+      oy + (size * 7) / 4,
+      (size * 2) / 16,
+      size / 4,
+    ],
     front0: [ox + size, oy + size * 2, size, size / 4],
-    left0: [ox + size * 2, oy + size * 7 / 4, (size * 2) / 16, size / 4],
+    left0: [ox + size * 2, oy + (size * 7) / 4, (size * 2) / 16, size / 4],
 
     top1: [ox + size, oy + (size * 14) / 16, size, (size * 2) / 16],
     bottom1: [ox + size, oy + (size * 20) / 16, size, (size * 2) / 16],
@@ -60,41 +65,83 @@ export function drawShelf(
 
   Face.defineInputRegion(generator, "ShelfFace" + blockId, regions.front);
 
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 0, 16, 16], regions.top);
   Face.drawFace(
     generator,
     "ShelfFace" + blockId,
-    [0, 0, 16, 16],
+    [8, 3.5, 8, 1.5],
+    regions.top
+  );
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [8, 4.5, 8, 1.5],
     regions.bottom
   );
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.right);
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.front);
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.left);
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.back);
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [8, 0, 1.5, 8],
+    regions.right
+  );
+  Face.drawFace(generator, "ShelfFace" + blockId, [8, 12, 8, 4], regions.front);
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [14.5, 0, 1.5, 8],
+    regions.left
+  );
+  Face.drawFace(generator, "ShelfFace" + blockId, [8, 0, 8, 8], regions.back);
 
   Face.defineInputRegion(generator, "ShelfFace" + blockId, regions.front);
 
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 0, 16, 16], regions.top0);
   Face.drawFace(
     generator,
     "ShelfFace" + blockId,
-    [0, 0, 16, 16],
+    [8, 3.5, 8, 1],
+    regions.top0,
+    { rotate: 180 }
+  );
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [8, 3.5, 8, 1],
     regions.bottom0
   );
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.right0);
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.front0);
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.left0);
-
-    Face.drawFace(generator, "ShelfFace" + blockId, [0, 0, 16, 16], regions.top1);
   Face.drawFace(
     generator,
     "ShelfFace" + blockId,
-    [0, 0, 16, 16],
-    regions.bottom1
+    [1.5, 6, 1, 2],
+    regions.right0
   );
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.right1);
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.front1);
-  Face.drawFace(generator, "ShelfFace" + blockId, [0, 8, 16, 8], regions.left1);
+  Face.drawFace(generator, "ShelfFace" + blockId, [0, 6, 8, 2], regions.front0);
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [5.5, 6, 1, 2],
+    regions.left0
+  );
+
+  Face.drawFace(generator, "ShelfFace" + blockId, [8, 5, 8, 1], regions.top1);
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [8, 5, 8, 1],
+    regions.bottom1,
+    { rotate: 180 }
+  );
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [1.5, 0, 1, 2],
+    regions.right1
+  );
+  Face.drawFace(generator, "ShelfFace" + blockId, [0, 0, 8, 2], regions.front1);
+  Face.drawFace(
+    generator,
+    "ShelfFace" + blockId,
+    [5.5, 0, 1, 2],
+    regions.left1
+  );
 
   generator.drawImage("Tabs-Shelf", [ox - 32, oy - 1]);
 
